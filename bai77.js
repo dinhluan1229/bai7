be884605a6508b9a4561e3391de0d1b3b35a3dbc
dinhluan1229 <leanhluan99xx99xx@gmail.com>
2022-10-12 16:48:08 +0700

    bai77

---
+++ bai77.js
@@ -0,0 +1,20 @@
+function add(){
+
+    let amountMoney = +document.getElementById('money').value;
+    let select1 = document.getElementById('select1').value;
+    let select2 = document.getElementById('select2').value;
+    let result = 0;
+    if (select1 === 'VND' && select2 === 'USD') {
+        result = amountMoney / 23000;
+    }
+    if (select1 === 'USD' && select2 === 'VND') {
+        result = amountMoney * 23000;
+    }
+    if (select1 === 'VNĐ' && select2 === 'VNĐ') {
+        result = amountMoney
+    }
+    if (select1 === 'USD' && select2 === 'USD') {
+        result = amountMoney
+    }
+    document.getElementById('result').innerHTML = result + '';
+}
